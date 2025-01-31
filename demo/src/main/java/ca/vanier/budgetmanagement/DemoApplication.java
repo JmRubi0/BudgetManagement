6d7e94252b9db92dc483ea597cc61cec99599bd6
JmRubi0 <johnmatthewrubio0530@gmail.com>
2025-01-30 22:37:21 -0500

    DemoApplication removed from the demo folder

--- demo/src/main/java/ca/vanier/budgetmanagement/DemoApplication.java
+++ demo/src/main/java/ca/vanier/budgetmanagement/DemoApplication.java
@@ -1,4 +1,4 @@
-package ca.vanier.budgetmanagement.demo;
+package ca.vanier.budgetmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
